fe5d81b59e554a93d9304c84bab6b7ca5b99c70c
Jeff Byrnes <jeff@evertrue.com>
2015-01-09 17:04:29 -0500

    Clarify verbiage for OpsCenter test assertions
    Reads more nicely in Serverspec/Rspec’s output

--- test/integration/opscenter/serverspec/default_spec.rb
+++ test/integration/opscenter/serverspec/default_spec.rb
@@ -1,27 +1,29 @@
 require 'spec_helper'
 
 describe 'DataStax OpsCenter' do
-  it 'has DataStax OpsCenter installed' do
+  it 'is installed' do
     expect(package('opscenter')).to be_installed
   end
 
-  it 'has enabled the OpsCenter service' do
+  it 'is enabled as a service' do
     expect(service('opscenterd')).to be_enabled
   end
 
-  it 'has a running server for OpsCenter' do
+  it 'is running as a service' do
     expect(service('opscenterd')).to be_running
   end
+end
 
-  it 'has the DataStax Agent installed' do
+describe 'DataStax Agent' do
+  it 'is installed' do
     expect(package('datastax-agent')).to be_installed
   end
 
-  it 'has enabled the DataStax Agent service' do
+  it 'is enabled as a service' do
     expect(service('datastax-agent')).to be_enabled
   end
 
-  it 'has a running service for the DataStax Agent' do
+  it 'is running as a service' do
     expect(service('datastax-agent')).to be_running
   end
 end
