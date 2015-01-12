39d9bf2199e82246c6eeef44eaf9c52c6e0beaca
Jeff Byrnes <jeff@evertrue.com>
2015-01-12 17:09:40 -0500

    Add tests for OpsCenter master config

--- test/integration/opscenter/serverspec/default_spec.rb
+++ test/integration/opscenter/serverspec/default_spec.rb
@@ -12,6 +12,26 @@ describe 'DataStax OpsCenter' do
   it 'is running as a service' do
     expect(service('opscenterd')).to be_running
   end
+
+  it 'has a directory for its cluster configs' do
+    expect(file('/etc/opscenter/clusters')).to be_directory
+  end
+
+  context 'has a cluster config' do
+    describe file '/etc/opscenter/clusters/test_cluster.conf' do
+      it { is_expected.to be_file }
+      describe '#content' do
+        subject { super().content }
+        it { is_expected.to include '[jmx]' }
+        it { is_expected.to include 'username = ' }
+        it { is_expected.to include 'password = ' }
+        it { is_expected.to include 'port = 7199' }
+        it { is_expected.to include '[cassandra]' }
+        it { is_expected.to include 'seed_hosts = 0.0.0.0' }
+        it { is_expected.to include 'api_port = 9160' }
+      end
+    end
+  end
 end
 
 describe 'DataStax Agent' do
