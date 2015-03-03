4cc05f1b1b8cbaf85acc082c63d94270696e278b
Jeff Byrnes <jeff@evertrue.com>
2015-03-03 18:18:09 -0500

    Ensure stronger ciphers are available to Java

--- attributes/default.rb
+++ attributes/default.rb
@@ -123,3 +123,4 @@ default['et_cassandra']['config'] = {
 default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '7'
 default['java']['oracle']['accept_oracle_download_terms'] = true
+default['java']['oracle']['jce']['enabled'] = true
