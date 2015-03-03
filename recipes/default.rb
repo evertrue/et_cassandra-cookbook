afe33d1e695c90fd4ace5f9c5204e8189248c83a
Jeff Byrnes <jeff@evertrue.com>
2015-03-03 11:30:53 -0500

    Add warning that this cookbook requires Chef 12

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,10 @@
 #
 # Copyright (c) 2014 EverTrue, Inc., All Rights Reserved.
 
+if Chef::VersionConstraint.new('< 12.0.0').include? Chef::VERSION
+  fail 'This recipe requires chef-client version 12.0.0 or higher'
+end
+
 include_recipe 'apt'
 include_recipe 'java'
 include_recipe 'et_cassandra::install'
