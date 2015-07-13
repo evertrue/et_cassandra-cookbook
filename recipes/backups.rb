2e7972cdd9000c7a60bd12eb1274b0f59ce7e53d
Eric Herot <eric.github@herot.com>
2015-07-13 18:24:38 -0400

    Add a backup/snapshot script

---
+++ recipes/backups.rb
@@ -0,0 +1,15 @@
+snapshot_conf = node['et_cassandra']['snapshot_conf'].to_h
+
+creds = data_bag_item('secrets', 'aws_credentials')['CassandraBackups']
+snapshot_conf['AWS_ACCESS_KEY_ID'] = creds['access_key_id']
+snapshot_conf['AWS_SECRET_ACCESS_KEY'] = creds['secret_access_key']
+
+file '/etc/cassandra/snapshots.conf' do
+  content snapshot_conf.map { |k, v| "#{k}=#{v}" }.join("\n") + "\n"
+  mode    0600
+end
+
+template '/usr/local/sbin/snapshot-cassandra' do
+  source 'snapshot-cassandra.erb'
+  mode   0755
+end
