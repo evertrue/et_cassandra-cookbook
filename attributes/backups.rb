b8cbba555508c6c4a5ebe08cd1fb389c8935ac6a
Eric Herot <eric.github@herot.com>
2015-07-24 15:34:35 -0400

    allow backup/snapshot scripts to set the S3 region

--- attributes/backups.rb
+++ attributes/backups.rb
@@ -1,3 +1,4 @@
 default['et_cassandra']['snapshot_conf']['BUCKET'] = 'cassandra_bucket'
+default['et_cassandra']['snapshot_conf']['REGION'] = 'us-east-1'
 default['et_cassandra']['snapshot']['data_bag'] = 'secrets'
 default['et_cassandra']['snapshot']['data_bag_item'] = 'aws_credentials'
