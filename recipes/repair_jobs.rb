7708bff2d90bafb4c22cae865e144bfec3ed9536
Eric Herot <eric.github@herot.com>
2015-08-07 01:08:20 -0400

    Disable repair job until we can figure out why it doesn't space correctly

--- recipes/repair_jobs.rb
+++ recipes/repair_jobs.rb
@@ -6,4 +6,6 @@ cron_d 'cassandra_daily_repair' do
   command '/usr/local/bin/cassandra-repair'
   minute  20
   hour    4
+  # Deleting this until we can figure out why jobs don't space properly
+  action :delete
 end
