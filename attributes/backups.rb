76a7befedb7e03dcf91ac6aea47c0c5231f7bb29
Eric Herot <eric.github@herot.com>
2015-08-12 12:01:17 -0400

    Put skip_keyspaces in the snapshots config file and add the OpsCenter keyspace to the list

--- attributes/backups.rb
+++ attributes/backups.rb
@@ -1,6 +1,8 @@
 default['et_cassandra']['snapshot_conf']['BUCKET'] = 'cassandra_bucket'
 default['et_cassandra']['snapshot_conf']['CHEF_ENVIRONMENT'] = node.chef_environment
 default['et_cassandra']['snapshot_conf']['REGION'] = 'us-east-1'
+default['et_cassandra']['snapshot_conf']['SKIP_KEYSPACES'] =
+  '(keyspace1 system_traces OpsCenter)'
 default['et_cassandra']['snapshot']['data_bag'] = 'secrets'
 default['et_cassandra']['snapshot']['data_bag_item'] = 'aws_credentials'
 default['et_cassandra']['snapshot']['snapshot_retention_days'] = 90
