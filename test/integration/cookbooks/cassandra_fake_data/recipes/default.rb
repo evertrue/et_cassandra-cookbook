052644e22a6936a3caddd42978d60af617cb8e69
Eric Herot <eric.github@herot.com>
2015-08-17 15:59:04 -0400

    Fix some overloaded filenames in the cassandra fake data cookbook

--- test/integration/cookbooks/cassandra_fake_data/recipes/default.rb
+++ test/integration/cookbooks/cassandra_fake_data/recipes/default.rb
@@ -1,5 +1,10 @@
-cookbook_file "#{Chef::Config[:file_cache_path]}/build_big_data" do
-  mode 0755
+[
+  "#{Chef::Config[:file_cache_path]}/build_big_data",
+  "#{Chef::Config[:file_cache_path]}/fakedata.cql"
+].each do |cb_file|
+  cookbook_file cb_file do
+    mode 0755
+  end
 end
 
 execute 'create_bogus_keyspace' do
@@ -10,7 +15,7 @@ end
 
 execute 'fill_bogus_keyspace' do
   command "#{Chef::Config[:file_cache_path]}/build_big_data 100000 > " \
-          '/tmp/fakedata.cql && cqlsh -f /tmp/fakedata.cql'
-  creates '/tmp/fakedata.cql'
+          '/tmp/loadfakedata.cql && cqlsh -f /tmp/loadfakedata.cql'
+  creates '/tmp/loadfakedata.cql'
   action  :run
 end
