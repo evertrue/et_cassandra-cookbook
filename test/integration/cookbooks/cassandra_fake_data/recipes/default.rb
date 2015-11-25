cb1d814d7589857869ed5b6af6379df011b280ab
Eric Herot <eric.github@herot.com>
2015-11-25 16:44:55 -0500

    Use cassandra-stress to generate data for testing

--- test/integration/cookbooks/cassandra_fake_data/recipes/default.rb
+++ test/integration/cookbooks/cassandra_fake_data/recipes/default.rb
@@ -1,21 +1,8 @@
-[
-  "#{Chef::Config[:file_cache_path]}/build_big_data",
-  "#{Chef::Config[:file_cache_path]}/fakedata.cql"
-].each do |cb_file|
-  cookbook_file cb_file do
-    mode 0755
-  end
-end
-
-execute 'create_bogus_keyspace' do
-  command "/usr/bin/cqlsh -f #{Chef::Config[:file_cache_path]}/fakedata.cql"
-  creates '/var/lib/cassandra/data/fakedata'
-  action  :run
-end
-
-execute 'fill_bogus_keyspace' do
-  command "#{Chef::Config[:file_cache_path]}/build_big_data 100000 > " \
-          '/tmp/loadfakedata.cql && cqlsh -f /tmp/loadfakedata.cql'
-  creates '/tmp/loadfakedata.cql'
+execute 'write_900k_rows' do
+  command 'cassandra-stress write n=900000 no-warmup'
+  creates '/var/lib/cassandra/data/keyspace1'
+  # Cassandra sometimes takes a few seconds to start up
+  retries 4
+  retry_delay 5
   action  :run
 end
