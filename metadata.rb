44946a16f0acce4e25e839ad3a604481501e99ad
Mark Greene <mark@evertrue.com>
2016-01-04 11:50:01 -0500

    Bump to v5.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'all_rights'
 description      'Installs/Configures a Cassandra ring w/ various goodies'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '4.8.14'
+version          '5.0.0'
 
 supports 'ubuntu', '= 14.04'
 
