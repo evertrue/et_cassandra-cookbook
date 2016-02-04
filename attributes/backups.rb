038403bd06c79f97c822948d6fa7d304777c8aa6
Eric Herot <eric.github@herot.com>
2016-02-03 19:17:44 -0500

    Make snapshot wait time an attribute

--- attributes/backups.rb
+++ attributes/backups.rb
@@ -5,4 +5,5 @@ default['et_cassandra']['snapshot_conf']['SKIP_KEYSPACES'] =
   '(keyspace1 system_traces OpsCenter)'
 default['et_cassandra']['snapshot']['data_bag'] = 'secrets'
 default['et_cassandra']['snapshot']['data_bag_item'] = 'aws_credentials'
+default['et_cassandra']['snapshot']['wait_secs'] = 30
 default['et_cassandra']['snapshot']['tar_retry_secs'] = 30
