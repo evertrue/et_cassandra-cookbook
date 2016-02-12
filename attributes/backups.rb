ee870950fdcb7951d589325dedda28771cab14e4
Eric Herot <eric.github@herot.com>
2016-02-12 16:36:07 -0500

    New parameter: upload_retry_delay

--- attributes/backups.rb
+++ attributes/backups.rb
@@ -7,3 +7,4 @@ default['et_cassandra']['snapshot']['data_bag'] = 'secrets'
 default['et_cassandra']['snapshot']['data_bag_item'] = 'aws_credentials'
 default['et_cassandra']['snapshot']['wait_secs'] = 30
 default['et_cassandra']['snapshot']['tar_retry_secs'] = 30
+default['et_cassandra']['incrementals']['upload_retry_delay'] = 5
