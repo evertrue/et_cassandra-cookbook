235a4cbbfa48601799882910b8d0c2ee0ae65312
Doug Youch <dougyouch@gmail.com>
2020-02-12 11:08:59 -0500

    use same java version as spark

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'all_rights'
 description      'Installs/Configures a Cassandra ring w/ various goodies'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '7.1.10'
+version          '7.1.11'
 
 supports 'ubuntu', '= 14.04'
 
@@ -12,7 +12,7 @@ chef_version '~> 12.10'
 
 # Prevent new JDK's from being installed. Old ones do not get cleaned up and large amounts of
 # disk space get used. Plus: C* is never restarted so security updates are mostly pointless.
-depends 'java', '~> 1.48.0'
+depends 'et_java', '~> 1.50.0'
 
 depends 'cron'
 depends 'poise-python'
