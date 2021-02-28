d780751612b34339d8c02c3ed7e522dd94fee1a3
Hai Zhou <hai.zhou@evertrue.com>
2021-02-28 00:26:25 -0500

    Specify poise-python options

--- attributes/default.rb
+++ attributes/default.rb
@@ -142,3 +142,8 @@ default['et_cassandra']['cronitor']['repairs']['enabled'] = false
 
 default['et_cassandra']['repairs']['timeout'] = 7200
 default['et_cassandra']['repairs']['retries'] = 5
+
+# python versions
+default['poise-python']['options']['get_pip_url'] = 'https://bootstrap.pypa.io/2.7/get-pip.py'
+default['poise-python']['options']['pip_version'] = '18.0'
+default['poise-python']['options']['virtualenv_version'] = '16.7.9'
